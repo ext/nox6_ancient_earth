92cef32d5b5315aa85af86364c06d4e9dd12b80a
David Sveningsson <david@horbergakerlund.se>
2015-07-11 16:14:31 +0200

    disable lightning

--- data/shader/default.fs
+++ data/shader/default.fs
@@ -7,8 +7,5 @@ in vec4 w_pos;
 out vec4 ocolor;
 
 void main(){
-	vec3 N = get_normal(uv);
-	vec4 color = texture2D(texture0, uv);
-
-	ocolor = calculate_light(color, w_pos.xy, N);
+	ocolor = texture2D(texture0, uv);
 }
